ff85ef0d91b39b202ffd2b911b80bcfe461b59f9
Natalia <pilewska.natalia@gmail.com>
2020-03-06 21:54:04 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,18 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import * as firebase from "firebase";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyChsSrkugmaRLygKR8IzGGFjT1EiaJmfFs",
-  authDomain: "travel-website-2fd73.firebaseapp.com",
-  databaseURL: "https://travel-website-2fd73.firebaseio.com",
-  projectId: "travel-website-2fd73",
-  storageBucket: "travel-website-2fd73.appspot.com",
-  messagingSenderId: "725745572723",
-  appId: "1:725745572723:web:1ae8d99d1f1da9e6"
-};
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
