03a8a7ac262e391d7b2e3350be4c59d510efa2a7
Natalia <pilewska.natalia@gmail.com>
2020-03-20 13:03:04 +0100

    like button added

--- src/components/Comments/DisplayComment/index.js
+++ src/components/Comments/DisplayComment/index.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faGratipay } from "@fortawesome/free-brands-svg-icons";
 
 export default function DisplayComment({ firstComment }) {
+  const [changeIcon, setChangeIcon] = useState(true);
+  const likeComment = () => {
+    setChangeIcon(false);
+  };
   return (
     <div className="showingComments">
       {firstComment.map((com, i) => {
         return (
           <div className="displaySingleComment">
             <p>{com.nickname}</p>
-            <p className="commentName">{com.comment}</p>
+            <div className="simpleComment">
+              <p className="commentName">{com.comment}</p>
+              <div className="commentLike" onClick={likeComment}>
+                {changeIcon ? (
+                  <FontAwesomeIcon
+                    icon={faGratipay}
+                    size="2x"
+                  ></FontAwesomeIcon>
+                ) : (
+                  <FontAwesomeIcon icon={faHeart} size="2x"></FontAwesomeIcon>
+                )}
+              </div>
+            </div>
           </div>
         );
       })}
